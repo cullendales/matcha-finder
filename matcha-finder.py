45ac3a9780d713ff46ea58b143a7e7c84d7caf5e
crd11 <crd11@sfu.ca>
2025-06-04 19:51:18 -0700

    add more code to API to increase functionality

--- matcha-finder.py
+++ matcha-finder.py
@@ -1,8 +1,10 @@
 import sqlite3
 import requests
+import time
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
+
 
 stop_words = set(stopwords.words('english'))
 nltk.download('punkt')
@@ -43,13 +45,12 @@ class MatchaFinder:
 
     def create_table(self):
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS matcha_lattes(
+            ID TEXT PRIMARY KEY
             NAME TEXT,
             ADDRESS TEXT,
             LON FLOAT,
             LAT FLOAT,
-            RATING REAL,
-            HAS_MATCHA BOOL,
-            KEYWORDS TEXT                                                            
+            RATING REAL                                                         
         );''')
         self.conn.commit()
 
@@ -60,17 +61,52 @@ class MatchaFinder:
         data = res.json()
         if data['status'] == 'OK':
             loc = data['results'][0]['geometry']['location']
-            return loc['lat'], loc['lon']
+            return loc['lat'], loc['lng']
         else:
-            raise Exception(f"Failed to get coordinates: {data['status']}")
+            raise Exception (f"Coordinates could not be retrieved: {data['status']}")
 
-    def search_matcha_cafes(self, lat, lon, radius=5000):
+    def search_matcha_cafes(self, lat, lon):
         url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
         params = {
             "key": self.api_key,
             "location": f"{lat},{lon}",
-            "radius": radius,
+            "radius": 10000,
             "type": "cafe",
-            "keyword": "matcha latte"
+            "keyword": "matcha latte"           
         }
+
+        cafes = []
+        max_cafes = 100
+
+        while True:
+            res = requests.get(url, params=params)
+            data = res.json()
+
+            if data.get("status") != "OK":
+                print("There is an API error: ", data.get("status"))
+                break
+
+            for result in data.get("results", []):
+                cafe = Cafe(
+                    place_id = result["place_id"],
+                    name = result.get("name", ""),
+                    address = result.get("vicinity", ""),
+                    rating = result.get("rating", 0),
+                    lat = result["geometry"]["location"]["lat"],
+                    lon = result["geometry"]["location"]["lng"]
+                )
+                cafes.append(cafe)
+                self.save_cafe(cafe)
+
+            if len(cafes) >= max_cafes:
+                return cafes
+
+            if 'next_page_token' in data:
+                time.sleep(2)
+                params['pagetoken'] = data['next_page_token']
+            else:
+                break
+            
+        return cafes
+     
 
